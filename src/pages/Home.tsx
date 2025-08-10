f20d557b022494c54a77faeaf0ade1417872fb70
Ashik Api <youremail@example.com>
2025-08-11 03:31:19 +0600

    add link in view work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,7 +57,7 @@ const Home = () => {
                         <button className='lg:px-4 lg:py-3 px-3 py-2 bg-[rgb(0,150,137)] hover:bg-teal-700 text-white font-bold 
                          text-lg rounded-md transform hover:transition hover:scale-110 duration-150'>Get In Touch</button>
                         <button className='lg:px-6 lg:py-2 px-3 py-2 text-lg border-gray-600 bg-gray-800 lg:bg-[rgb(255,255,255)] font-bold text-white border-2 hover:border-gray-600 
-                        rounded-md hover:bg-gray-800 transform hover:transition hover:scale-110 duration-150'>View Work</button>
+                        rounded-md hover:bg-gray-800 transform hover:transition hover:scale-110 duration-150'><a href="#project">View Work</a></button>
                     </div>
                     {/* find me in button */}
                     <div className='flex justify-between mt-2 mb-6'>
