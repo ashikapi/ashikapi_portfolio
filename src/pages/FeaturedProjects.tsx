c9ba45cfe1d1d505ca20cf9894d6c7dbddb6dd8d
Ashik Api <ashikapi65@gmail.com>
2026-01-17 20:44:59 +0600

    update

--- src/pages/FeaturedProjects.tsx
+++ src/pages/FeaturedProjects.tsx
@@ -1,30 +1,43 @@
 import { TbWindowMinimize } from "react-icons/tb";
+import projectImage1 from '../images/project_image/bmcbd.png';
 
 const featuredetails = [
   {
     title: 'DigiKart – Gift Card & Game Top-Up Platform',
 Description: 'Dynamic gift card platform featuring OTP login and SSLCommerz checkout.',
-Tech: ['PHP', 'MySQL', 'HTML', 'CSS', 'Java Script', 'PHP Mailer']
+Tech: ['PHP', 'MySQL', 'HTML', 'CSS', 'Java Script', 'PHP Mailer'],
+url: ''
   },
   {
     title: 'HRMS – Human Resource Management System',
 Description: 'Internship project for HR Management UI using React & Tailwind CSS.',
-Tech: ['React', 'Type Script', 'Tailwind CSS']
+Tech: ['React', 'Type Script', 'Tailwind CSS'],
+url: '#'
   },
   {
     title: 'Fashion--House (Online Shop)',
     Description: 'Modern fashion e-commerce UI with responsive design.',
-    Tech: ['React', 'Type Script', 'Tailwind CSS']
+    Tech: ['React', 'Type Script', 'Tailwind CSS'],
+    url: '#'
   },
-  {
-   title: 'Farmer Ecommerce Frontend',
-Description: 'Responsive product listing UI for agricultural e-commerce.',
-Tech:['React', 'Type Script', 'Tailwind CSS']
-  },
-  {
-    title: 'Digital Agency Landing Page',
-Description: 'Single page landing site with scroll animation & responsive layout.',
-Tech:[ 'HTML', 'CSS']
+//   {
+//    title: 'Farmer Ecommerce Frontend',
+// Description: 'Responsive product listing UI for agricultural e-commerce.',
+// Tech:['React', 'Type Script', 'Tailwind CSS'],
+// url: '#'
+//   },
+//   {
+//     title: 'Digital Agency Landing Page',
+// Description: 'Single page landing site with scroll animation & responsive layout.',
+// Tech:[ 'HTML', 'CSS'],
+// url: '#'
+//   },
+   {
+    img: projectImage1,
+    title: 'BMCBD – CIPS-Focused Corporate Website',
+Description: 'A frontend-developed corporate website for a CIPS-focused organization, featuring responsive layout, structured content, and optimized performance.',
+Tech:[ 'React', 'Tailwind CSS', 'Type Script' ],
+url: 'https://bmcbd.netlify.app/'
   }
 ]
 
@@ -37,15 +50,19 @@ const FeaturedProjects = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4'>
                 {featuredetails.map((project, index) => (
-            <div key={index} className='flex flex-col items-center bg-white text-white p-4 shadow-sm shadow-gray-300 rounded-xl'>
+            <div key={index} className='flex flex-col items-center bg-white text-white shadow-sm shadow-gray-300 rounded-xl overflow-hidden hover:shadow-lg hover:shadow-gray-400 transition-shadow duration-300'>
+              <img src={project.img} alt={project.title} className="rounded-t-xl mb-4 transform transition-all hover:scale-105 duration-700 object-cover" />
+              <div className="p-4">
                 <h1 className='text-xl font-semibold text-black mb-3'>{project.title}</h1>
                 <p className='text-center text-black opacity-80'>{project.Description}</p>
-                <div className='grid grid-cols-4 gap-2 mt-4 text-[rgb(33,156,137)]'>
+                <div className='grid grid-cols-4 gap-2 mt-4 text-[rgb(33,156,137)] pb-10'>
                   {project.Tech.map((tech, index) => (
                 <button key={index} className='px-2 py-1 bg-[rgb(203,251,241)] rounded-md'>{tech}</button>
                   ))}
                 </div>
-                <button className='flex justify-center items-center text-center mt-10 w-full p-2 border border-[rgb(90,185,177)] text-[rgb(8,157,153)] rounded-xl gap-4 text-xl hover:bg-teal-50 hover:text-black font-semibold'><TbWindowMinimize /> Visit Now</button>
+                
+                <button className='flex justify-center items-center text-center mt-auto w-full p-2 border border-[rgb(90,185,177)] text-[rgb(8,157,153)] rounded-xl gap-4 text-xl hover:bg-teal-50 hover:text-black font-semibold'><a href={project.url} target="_blanck" className="flex justify-evenly items-center gap-2"><TbWindowMinimize /> Visit Now</a></button>
+                </div>
                 </div>
                 ))}
             </div>
